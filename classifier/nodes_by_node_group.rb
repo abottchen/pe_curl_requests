d62af44dc4bcb4207d10e714ef18648135386197
Adam Bottchen <adam.bottchen@puppet.com>
2017-05-03 14:46:23 -0700

    Making puppetdb and console parameters for nodes_by_node_group.rb

--- classifier/nodes_by_node_group.rb
+++ classifier/nodes_by_node_group.rb
@@ -14,7 +14,7 @@ def curl_data(command)
   retcode = $?.exitstatus
   if(retcode != 0) then
     if(retcode == 7) then
-      puts "ERROR: Unable to reach #{command.match(/(https.*)/)}"
+      puts "ERROR: Unable to reach #{command.match(/(http.*)/)}"
     else
       puts "ERROR: Failed to curl endpoint, retcode=#{retcode}"
     end
@@ -49,7 +49,7 @@ def get_rules(arr, group_id,first=true)
       if(group['rule'].to_s != '') then
         debug "Translating rule '#{group['rule'].to_s}' for group '#{group['name']}'"
         
-        data = curl_data("curl -s -X POST --cert $(puppet config print hostcert) --key $(puppet config print hostprivkey) --cacert $(puppet config print localcacert) https://#{CONSOLE_NODE_FQDN}:4433/classifier-api/v1/rules/translate -H 'Content-Type: application/json' --data '#{group['rule'].to_s}'")
+        data = curl_data("curl -s -X POST --cert $(puppet config print hostcert) --key $(puppet config print hostprivkey) --cacert $(puppet config print localcacert) https://#{$options[:console]}:4433/classifier-api/v1/rules/translate -H 'Content-Type: application/json' --data '#{group['rule'].to_s}'")
         parsed_rules = JSON.parse(data)
 
         debug "Adding rules translated rules '#{parsed_rules['query'].to_s}'"
@@ -93,10 +93,10 @@ def build_query_string(arr)
 end
 
 # main
-CONSOLE_NODE_FQDN = `hostname -f`.chomp
-PUPPETDB_NODE_FQDN = "localhost"
 
 $options = {}
+$options[:console] = `hostname -f`.chomp
+$options[:pdb] = $options[:console]
 OptionParser.new do |opts|
   opts.banner = "Usage: nodes_by_node_group.rb [flags] <Group Name> or nodes_by_node_group.rb --list"
   opts.on("-d","--debug","Enable debug output") do |d|
@@ -105,9 +105,15 @@ OptionParser.new do |opts|
   opts.on("-l","--list","List available groups") do |l|
     $options[:list] = l
   end
+  opts.on("-p","--puppetdb <hostname>","Fully qualified hostname of the PuppetDB node (Required on split installs)") do |p|
+    $options[:pdb] = p
+  end
+  opts.on("-c","--console <hostname>","Fully qualified hostname of the Console node (Required on split installs)") do |c|
+    $options[:console] = c
+  end
 end.parse!
 
-data = curl_data("curl -s --cert $(puppet config print hostcert) --key $(puppet config print hostprivkey) --cacert $(puppet config print localcacert) https://#{CONSOLE_NODE_FQDN}:4433/classifier-api/v1/groups")
+data = curl_data("curl -s --cert $(puppet config print hostcert) --key $(puppet config print hostprivkey) --cacert $(puppet config print localcacert) https://#{$options[:console]}:4433/classifier-api/v1/groups")
 $groups = JSON.parse(data)
 
 if($options[:list]) then
@@ -124,14 +130,14 @@ end
 
 id_hash = build_id_to_name
 if !(id_hash.has_key?($group_to_find)) then
-  puts "ERROR: No group named '#{$group_to_find}' present on #{CONSOLE_NODE_FQDN}."
+  puts "ERROR: No group named '#{$group_to_find}' present on #{$options[:console]}."
   exit -1
 end
 
 get_rules(arr = [], id_hash[$group_to_find])
 query_string = build_query_string(arr)
 
-data = curl_data("curl -s -G http://#{PUPPETDB_NODE_FQDN}:8080/pdb/query/v4/nodes --data-urlencode 'query=#{query_string}'")
+data = curl_data("curl -s --cert $(puppet config print hostcert) --key $(puppet config print hostprivkey) --cacert $(puppet config print localcacert) -G https://#{$options[:pdb]}:8081/pdb/query/v4/nodes --data-urlencode 'query=#{query_string}'")
 nodes = JSON.parse(data)
 
 title = "Nodes for the class '#{$group_to_find}'"
