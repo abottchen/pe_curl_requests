d8803211cc4c1fc8fcebd7512a4d2762ec716ba8
Adam Bottchen <abottchen@gmail.com>
2018-04-02 10:48:29 -0700

    Update explain.sh

--- classifier/explain.sh
+++ classifier/explain.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 HOSTNAME=machineto.look.up curl -X POST --cert $(puppet config print hostcert) --key $(puppet config print hostprivkey) --cacert $(puppet config print localcacert) https://$(hostname -f):4433/classifier-api/v1/classified/nodes/$HOSTNAME/explanation --data "$(ssh $HOSTNAME puppet facts |egrep -v '(timestamp":|expiration":)' |sed -e 's/"values":/"fact":/' |sed -e 's/^  },$/  }/')" -H "Content-Type: application/json"
+
+# This one will query for the last facts submitted.  Can be run on the AIO master without needing an ssh login to pull the facts
+HOSTNAME=machineto.look.up curl -X POST --cert $(puppet config print hostcert) --key $(puppet config print hostprivkey) --cacert $(puppet config print localcacert) https://$(hostname -f):4433/classifier-api/v1/classified/nodes/$(hostname -f)/explanation --data "$(puppet query 'inventory[certname,facts] { certn
+ame = $HOSTNAME }' |tail -n +2 |head -n -1 |sed -e 's/^  //' |sed -e 's/^  "certname":/  "name":/' |sed -e 's/^  "facts"/  "fact"/')" -H "Content-Type: application/json" 
