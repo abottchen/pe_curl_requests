b06d430005a1358b9c9e3f9d75159c9f0da5162b
Adam Bottchen <abottchen@gmail.com>
2019-04-29 14:43:18 -0700

    Update get_users.sh

--- rbac/get_users.sh
+++ rbac/get_users.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --cert $(puppet config print hostcert) --key $(puppet config print hostprivkey)  --cacert $(puppet config print localcacert) https://$(grep server /etc/puppetlabs/puppet/classifier.yaml |cut -d" " -f 2):4433/rbac-api/v1/users
+curl --cert $(puppet config print hostcert) --key $(puppet config print hostprivkey)  --cacert $(puppet config print localcacert) https://$(hostname -f):4433/rbac-api/v1/users
